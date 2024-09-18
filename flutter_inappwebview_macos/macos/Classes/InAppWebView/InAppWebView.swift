c060ea254eec9c63eee497bb3b265e0f23497438
saminndex <sam@inndex.co.uk>
2024-09-18 09:14:56 +0100

    fix

--- flutter_inappwebview_macos/macos/Classes/InAppWebView/InAppWebView.swift
+++ flutter_inappwebview_macos/macos/Classes/InAppWebView/InAppWebView.swift
@@ -867,7 +867,7 @@ public class InAppWebView: WKWebView, WKUIDelegate,
         }
     }
     
-    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, (any Error)?) -> Void)? = nil) {
+    public func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, (any Error)?) -> Void)? = nil) {
         if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
             completionHandler?(nil, nil)
             return
